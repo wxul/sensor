7a0d38cab7aa5c8385ceba198655a7609d459b9d
albert <wlj_3322@163.com>
2017-12-17 17:53:35 +0800

    移除node_modules

---
+++ README.md
@@ -0,0 +1,9 @@
+## 树莓派
+
+#### 命令
+
+- `python py/dht11_w.py`: 温湿度传感器
+- `python py/pump.py`: 水泵
+- 待定
+
+- `node js/pump.js`: 水泵
---
+++ js/pump.js
@@ -0,0 +1,15 @@
+var rpio = require('rpio');
+
+var pin1 = 38;
+var pin2 = 40;
+
+rpio.open(pin1, rpio.OUTPUT);
+rpio.open(pin2, rpio.OUTPUT);
+
+rpio.write(pin1, rpio.HIGH);
+rpio.write(pin2, rpio.LOW);
+
+rpio.sleep(2);
+
+rpio.close(pin1);
+rpio.close(pin2);
