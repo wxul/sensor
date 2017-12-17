6322415ed68a0fc28df430c1e07517f30f56e9a6
albert <wlj_3322@163.com>
2017-12-17 20:31:13 +0800

    相机2

--- src/exec/camera.js
+++ src/exec/camera.js
@@ -1,5 +1,7 @@
 var exec = require('child_process').exec;
 var path = require('path');
+var fs = require('fs');
+// var Bufffer = require('Buffer');
 
 function Camera() {}
 
@@ -9,14 +11,24 @@ Camera.prototype.takePhoto = function() {
         var dht = exec('raspistill -q 100 -t 0 -e png -o -', {
             maxBuffer: 10 * 1024 * 1024
         });
+        var responseData = [];
         dht.stdout.on('data', function(data) {
-            console.log(typeof data, data);
+            // console.log(typeof data, data);
+            responseData.push(Buffer.from(data));
         });
         dht.stderr.on('data', function(data) {
             reject(data);
         });
         dht.on('exit', function(code) {
-            console.log('exit:', code);
+            // console.log('exit:', code);
+            var finalData = Buffer.concat(responseData);
+            fs.writeFile(
+                path.resolve(__dirname, '../../test.png'),
+                finalData,
+                e => {
+                    console.log(e);
+                }
+            );
             resolve(true);
         });
     });
