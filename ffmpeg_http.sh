54fbf2019be94d3dbfbdf096f9bd7cba7435c120
WuLiangJie <wlj_3322@163.com>
2018-01-10 11:30:04 +0800

    修改http

--- ffmpeg_http.sh
+++ ffmpeg_http.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -f v4l2 -r 30 -video_size 640x480 -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f mpeg1video http://127.0.0.1:8081/whosyourdaddy
+ffmpeg -f v4l2 -r 30 -video_size 640x480 -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f flv http://127.0.0.1:8081/whosyourdaddy
