2d722334a0779b0528a018132f68071b167ada21
WuLiangJie <wlj_3322@163.com>
2018-01-11 10:41:53 +0800

    提高视频质量

--- ffmpeg_rtmp.sh
+++ ffmpeg_rtmp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -f v4l2 -r 20 -video_size vga -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f flv rtmp://127.0.0.1/hls/camera
+ffmpeg -f v4l2 -r 30 -video_size 1280×720 -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f flv rtmp://127.0.0.1/hls/camera
