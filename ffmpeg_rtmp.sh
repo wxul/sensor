502c090b93b12756431b734206fbd3d56ee8b589
WuLiangJie <wlj_3322@163.com>
2018-01-11 11:01:44 +0800

    更新命令

--- ffmpeg_rtmp.sh
+++ ffmpeg_rtmp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -f v4l2 -r 30 -video_size hd720 -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f flv rtmp://127.0.0.1/hls/camera
+ffmpeg -f v4l2 -r 25 -s 1920x1080 -video_size hd720 -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f flv rtmp://127.0.0.1/hls/camera
