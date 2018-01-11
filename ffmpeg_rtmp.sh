b11217edd090f5cddd35b63f4184c5ce128e5824
WuLiangJie <wlj_3322@163.com>
2018-01-11 16:28:14 +0800

    样式

--- ffmpeg_rtmp.sh
+++ ffmpeg_rtmp.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+## docs:https://ffmpeg.org/ffmpeg-all.html#video4linux2_002c-v4l2
 ffmpeg -f v4l2 -r 25 -video_size hd720 -pix_fmt yuv420p -i /dev/video0 -b:v 500k -c:v h264_omx -an -f flv rtmp://127.0.0.1/hls/camera
