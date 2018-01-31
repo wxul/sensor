25d30d373a3e009be07a79d960c6a5378c834780
albert <wlj_3322@163.com>
2018-01-31 11:38:44 +0800

    添加浇水

--- scripts/pump.js
+++ scripts/pump.js
@@ -1,17 +1,22 @@
-var rpio = require('rpio');
+module.exports.begin = function() {
+    try {
+        var rpio = require('rpio');
 
-if (!rpio) throw new Error('rpio is required!!!');
+        if (!rpio) throw new Error('rpio is required!!!');
 
-var pin1 = 38;
-var pin2 = 40;
+        var pin1 = 38;
+        var pin2 = 40;
+        rpio.open(pin1, rpio.OUTPUT);
+        rpio.open(pin2, rpio.OUTPUT);
 
-rpio.open(pin1, rpio.OUTPUT);
-rpio.open(pin2, rpio.OUTPUT);
+        rpio.write(pin1, rpio.HIGH);
+        rpio.write(pin2, rpio.LOW);
 
-rpio.write(pin1, rpio.HIGH);
-rpio.write(pin2, rpio.LOW);
+        rpio.sleep(10);
 
-rpio.sleep(2);
-
-rpio.close(pin1);
-rpio.close(pin2);
+        rpio.close(pin1);
+        rpio.close(pin2);
+    } catch (error) {
+        console.log(error);
+    }
+};
--- web/route/sensor/SensorService.js
+++ web/route/sensor/SensorService.js
@@ -1,8 +1,18 @@
 const light = require('../../../scripts/light2');
+const pump = require('../../../scripts/pump');
+
+const { pump_key } = require('../../../app_config');
 
 module.exports = {
     light: (req, res) => {
         light.turnOn();
+        res.end('success');
+    },
+    pump: (req, res) => {
+        var k = req.query.key;
+        if (k == pump_key) {
+            setImmediate(pump.begin);
+        }
         res.end('success');
     }
 };
